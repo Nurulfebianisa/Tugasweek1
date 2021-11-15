7ffe260f003200f70b647f21f26e3c33acd69e28
Nurulfebianisa <92684819+Nurulfebianisa@users.noreply.github.com>
2021-11-15 17:26:15 +0700

    Add files via upload

---
+++ src/BiodataDiri.kt
@@ -0,0 +1,19 @@
+fun main(){
+
+    val Nama: String = "Nurul Febi Anisa"
+    val TTL : String = "Tegal, 05 Februari 2002"
+    val Kelas: String = "1C"
+    val Prodi: String = "Teknik Informatika"
+
+    println(Nama)
+    println(TTL)
+    println(Kelas)
+    println(Prodi)
+
+    println(Nama.length)
+    println(TTL.length)
+    println(Kelas.length)
+    println(Prodi.length)
+}
+
+
